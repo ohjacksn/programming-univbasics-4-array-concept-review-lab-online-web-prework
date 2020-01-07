b85278d5411b30e457f166b52b29421a0ab72993
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-07 03:55:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,12 +10,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.length.time do |count|
-    if array[count] == value_to_find
-      return count
+  x = array[0]
+  array.length.times { |index|
+    if array[index] > x
+      x = array[index]
     end
-    nil
-  end
+  }
+  x
 end
 
 def find_min_value(array)
